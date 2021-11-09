f57e2c973f24163db0c81536de598c2c0f903133
Yeraldo Nicolás Moreira Valdivieso <nicola1994@hotmail.es>
2021-11-09 00:35:57 -0500

    Add email validator

---
+++ script/script.js
@@ -0,0 +1,18 @@
+const MAIL_FORMAT = /(\S+)@(\S+)\.(\S+)/;
+
+const btnSubmit = document.querySelector("#submit-email");
+const emailBox = document.querySelector("#email");
+const errorIcon = document.querySelector(".error-icon");
+const invalidText = document.querySelector(".invalid");
+
+btnSubmit.addEventListener("click", validateMail);
+
+function validateMail() {
+  if (emailBox.value.match(MAIL_FORMAT)) {
+    invalidText.classList.add("hidden");
+    errorIcon.classList.add("hidden");
+  } else if (!emailBox.value.match(MAIL_FORMAT) || emailBox.value === "") {
+    invalidText.classList.remove("hidden");
+    errorIcon.classList.remove("hidden");
+  }
+}
